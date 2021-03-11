a7948cd76eeb28c4939d65a691e0c167a61b56a5
Kelly Hill <kelly.m.hill2115@gmail.com>
2021-03-10 21:03:12 -0600

    commit-3

---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/work">Work</router-link> | 
+      <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
