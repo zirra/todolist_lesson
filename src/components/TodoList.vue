f7a775218673434ef999c7f82484f764eb8e8552
Kelly Hill <kelly.m.hill2115@gmail.com>
2021-03-14 21:49:38 -0500

    Added blog body

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="todo-list">
-    <h1>Todo list</h1>
-    
+    <h1>Super Hero Posts</h1> <br>
+
+    <h2>How did you Taekwon-do that???</h2>
+    <h5>March 14th, 2021 / LEAVE A COMMENT</h5>
+
+    <p>I first began Taekwondo On November 10th, 2020 for two reasons: 1st reason out of sheer boredom and the 2nd reason was that my cousin announced on Facebook that he just got his blackbelt after 4.5 years. Sooooo naturally I guess I thought it was time for me to take part. From November through March 4th, I trained as a white belt and earned my skill stripes accordingly. On March 5th, I tested for my yellow belt and passed! I am currenly a yellow belt in training! I am having so much fun. I thoroughly enjoy moving my body.</p>
+
+
+    <h2>Lets Go For A Ride</h2>
+    <h5>March 14th, 2021 / LEAVE A COMMENT</h5>
+
+    <p>I got into dirt biking literally right after watching this show called Charm City Kings lol. That was pretty much the breaking point. I good friend of mine rides/races bikes all the time. He's shared pics and videos of his experiences with me over the past couple of years. Intrigued by this, I was unknowling developing an interest for riding bikes....dirt bikes that is. I had my first lesson on January Saturday, January 16th, 2021 and I never looked back. I had my second ride on Saturday, March 6th, 2021. I think it's safe to say that I'm addicted lol. I can't wait to do it again!!!</p>
+
   </div>
 </template>
 
@@ -10,5 +21,22 @@
 </script>
 
 <style>
+h1, h2, h5 {
+  display: flex;
+  justify-content: left;
+}
+h2 {
+  color: blue;
+}
+
+p {
+  display: flex;
+  justify-content: left;
+}
+
+h5 {
+  margin-top: -15px;
+  color: red;
+}
 
 </style>
