317426daffe2a813b602b975420363c8a2a5aecc
Kelly Hill <kelly.m.hill2115@gmail.com>
2021-03-15 19:05:58 -0500

    commit edited text

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,13 +5,18 @@
     <h2>How did you Taekwon-do that???</h2>
     <h5>March 14th, 2021 / LEAVE A COMMENT</h5>
 
-    <p>I first began Taekwondo On November 10th, 2020 for two reasons: 1st reason out of sheer boredom and the 2nd reason was that my cousin announced on Facebook that he just got his blackbelt after 4.5 years. Sooooo naturally I guess I thought it was time for me to take part. From November through March 4th, I trained as a white belt and earned my skill stripes accordingly. On March 5th, I tested for my yellow belt and passed! I am currenly a yellow belt in training! I am having so much fun. I thoroughly enjoy moving my body.</p>
+    <p>I first began Taekwondo on November 10th, 2020 for two reasons: the 1st reason was out of sheer boredom and the 2nd reason was that my cousin recently announced on Facebook that he earned his blackbelt after 4.5 years. Sooooo naturally I guess I thought it was time for me to commit myself to the martial arts. From November through March 4th, I trained as a white belt and earned my skill stripes accordingly. On March 5th, I tested for my yellow belt and passed! I am currenly a yellow belt in training! I am having so much fun and I thoroughly enjoy moving my body.</p>
 
 
     <h2>Lets Go For A Ride</h2>
     <h5>March 14th, 2021 / LEAVE A COMMENT</h5>
 
-    <p>I got into dirt biking literally right after watching this show called Charm City Kings lol. That was pretty much the breaking point. I good friend of mine rides/races bikes all the time. He's shared pics and videos of his experiences with me over the past couple of years. Intrigued by this, I was unknowling developing an interest for riding bikes....dirt bikes that is. I had my first lesson on January Saturday, January 16th, 2021 and I never looked back. I had my second ride on Saturday, March 6th, 2021. I think it's safe to say that I'm addicted lol. I can't wait to do it again!!!</p>
+    <p>I got into dirt biking literally right after watching this show called Charm City Kings lol. That was pretty much the breaking point. A good friend of mine rides/races bikes all the time. He's shared pics and videos of his experiences with me over the past couple of years. Intrigued by this, I was unknowling developing an interest for riding bikes....dirt bikes that is. I had my first lesson on Saturday, January 16th, 2021 and I never looked back. I had my second ride on Saturday, March 6th, 2021. I think it's safe to say that I'm addicted lol. I can't wait to do it again!!!</p>
+
+    <h2>Get in Fit-Tech Shape</h2>
+    <h5>March 14th, 2021 / LEAVE A COMMENT</h5>
+
+    <p>I love fitness and technology! The combination of the two are majestic and are great ways to keep my body and mind fit. Being physically fit, strong and creative are my super powers. BOOM!! POW!!</p>
 
   </div>
 </template>
